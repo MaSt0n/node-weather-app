e750b5e34bd33a5c4289a70ecba60272c6e12245
mazhun <realmazhun@gmail.com>
2020-03-25 15:54:26 +0800

    change

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require("request")
 
 const forecast = (latitude, longitude, callback) => {
-  const url = 'https://api.darksky.net/forecast/90a4a7d76d4d12b63aa697d902af88b6/' + latitude + ',' + longitude + '?units=si'
+  const url = 'https://api.darksky.net/forecast/90a4a7d76d4d12b63aa697d902af88b6/' + latitude + ',' + longitude + '?units=si&lang=zh'
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location.', undefined)
     } else {
-      callback(undefined, body.daily.data[0].summary + ' It is cerrently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+      callback(undefined, body.daily.data[0].summary + ' 室外温度为 ' + body.currently.temperature + ' 摄氏度. 有 ' + body.currently.precipProbability + '% 的降雨概率.')
     }
   })
 }
